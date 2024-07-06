6117761cb4f4d999768fa751c1929381bf01b03b
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-07-06 15:08:24 +0200

    hotfix: try to add the docstring file

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     url="https://github.com/thiswillbeyourgithub/py_ankiconnect",
     packages=find_packages(),
     include_package_data=True,
+    package_data={"py-ankiconnect": ["./py_ankiconnect/help.md"]}
 
     classifiers=[
         "Programming Language :: Python :: 3",
