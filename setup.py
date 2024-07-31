137eb8d8162196797ad57d5b6544d02be456519b
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-07-31 18:12:33 +0200

    bump version 1.0.2 -> 1.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="py-ankiconnect",
-    version="1.0.2",
+    version="1.1.0",
     description="Minimal wrapper to simplify the usage of the awesome ankiconnect anki addon.",
     long_description=long_description,
     long_description_content_type="text/markdown",
